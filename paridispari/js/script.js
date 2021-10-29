1822bb80d1aafe19d22c66d20a5109cf715bca42
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-29 16:20:26 +0200

    pseudocodice

---
+++ paridispari/js/script.js
@@ -0,0 +1,6 @@
+// 1. chiedere all'utente di scommetere se uscirà pari o dispari
+// 2. chiediamo all'utente di inserire un numero compreso tra 1 a 5
+// 3. generiamo un numero casuale da 1 a 5 (funz)
+// 4. sommiamo i 2 numeri
+// 5. somma pari o dispari? (funz)
+// 6. stampiamo il vincitore
