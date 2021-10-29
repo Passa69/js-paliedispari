147f4a0342bce65b1fcf37e34bff35a9134d64df
Tommaso Passarotto <passarotto13@gmail.com>
2021-10-29 16:11:01 +0200

    spiegazione dei passaggi

--- palindrome/js/script.js
+++ palindrome/js/script.js
@@ -1,6 +1,14 @@
-// chiedo all'utente una parola
-// funzione per capire se la parola è palindroma
-// output con relativa conferma o meno
+// 1. chiedo all'utente una parola
+// 2. funzione per capire se la parola è palindroma
+    // 2.1 creo una funzione e le do un nome e una variabile
+    // 2.2 creo una variabile al suo interno e la sovrascrivo alla varibile iniziale 
+        // 2.21 divido la parola in lettere
+        // 2.22 posiziono le lettere al contrario
+        // 2.23 riunisco la parola
+        // 2.24 ritorno il risultato
+    // 2.3 creo un ulteriore variabile in cui sovrascrivo la funzione aggiungendo la parola dell'utente
+// 3. output con relativa conferma o meno
+    // 3.1 se la parola inserita dall'utente rimane uguale allora è palindroma
+    // 3.2 se la parola inserita dall'utente cambia allora non è palindroma
 
 
-const userWord = prompt("Inserire una parola: ")
